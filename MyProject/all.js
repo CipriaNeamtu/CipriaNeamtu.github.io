d38cf2e44db104da558d69387de7ba7acccdb133
CipriaNeamtu <ciprian.neamtu91@gmail.com>
2021-03-06 12:26:26 +0200

    Freeze Header

--- MyProject/all.js
+++ MyProject/all.js
@@ -39,3 +39,17 @@ function topFunction() {
 topButton.onclick = () => {
     topFunction();
 }
+
+
+window.onscroll = function () { myFunction() };
+
+let header = document.getElementById("myHeader");
+let sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
